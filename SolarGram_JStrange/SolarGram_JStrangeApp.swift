3d87d330e2b500e481d753a53b01b9bd221dc003
Janae Strange <janaestr@usc.edu>
2023-03-03 15:34:58 -0700

    Initial Commit

---
+++ SolarGram_JStrange/SolarGram_JStrangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarGram_JStrangeApp.swift
+//  SolarGram_JStrange
+//
+//  Created by Janae Strange on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SolarGram_JStrangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
